4d32351423fa2da6bbb3a4edf3a0440df1cb5281
Ahtisham khan Mughal <09-131242-014@student.bahria.edu.pk>
2026-04-22 00:33:44 +0500

    setting up mongoose

--- airbab/app.js
+++ airbab/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(rootdir, "public")));
 app.use(ErrorController.pageNotFound);
 
 const PORT = 3000;
-const DB_Path = "mongodb://akhano_db_user:8787@ac-luxmqei-shard-00-00.1pqgdir.mongodb.net:27017,ac-luxmqei-shard-00-01.1pqgdir.mongodb.net:27017,ac-luxmqei-shard-00-02.1pqgdir.mongodb.net:27017/Airbnb?authSource=admin&tls=true";
+const DB_Path = "mongodb://akhano_db_user:1515@ac-luxmqei-shard-00-00.1pqgdir.mongodb.net:27017,ac-luxmqei-shard-00-01.1pqgdir.mongodb.net:27017,ac-luxmqei-shard-00-02.1pqgdir.mongodb.net:27017/Airbnb?authSource=admin&tls=true";
 mongoose.connect(DB_Path)
 .then(()=>{
   console.log("Mongo DB Connected")
