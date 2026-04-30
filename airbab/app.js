6533805ce4f1a194cb70ff23b655e03d72b6ee00
Ahtisham khan Mughal <09-131242-014@student.bahria.edu.pk>
2026-05-01 03:05:22 +0500

    Clear the express-session

--- airbab/app.js
+++ airbab/app.js
@@ -7,20 +7,27 @@ const authrouter = require("./routes/authRouter");
 const rootdir = require("./utils/pathUtils");
 const ErrorController = require("./controllers/error");
 const mongoose = require("mongoose");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const DB_Path = "mongodb://localhost:27017/Airbab";
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-app.use(express.urlencoded({ extended: false }));
+const store = new MongoDBStore({
+  uri: DB_Path,
+  collection: "sessions",
+});
 
+app.use(express.urlencoded({ extended: false }));
 app.use(
   session({
     secret: "my-airbnb-secret-key",
     resave: false,
     saveUninitialized: false,
-  })
+    store:store
+  }),
 );
 
 // Make isLoggedIn available to all views
@@ -39,13 +46,15 @@ app.use(express.static(path.join(rootdir, "public")));
 app.use(ErrorController.pageNotFound);
 
 const PORT = 3000;
-const DB_Path = "mongodb://localhost:27017/Airbab";
-mongoose.connect(DB_Path)
-.then(()=>{
-  console.log("Mongo DB Connected")
-  app.listen(PORT, () => {
-    console.log(`Server running on address http://localhost:${PORT}`);
-  });
-})
-.catch(err=>{console.log(" Mongo DB Connection Error: ",err)});
 
+mongoose
+  .connect(DB_Path)
+  .then(() => {
+    console.log("Mongo DB Connected");
+    app.listen(PORT, () => {
+      console.log(`Server running on address http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(" Mongo DB Connection Error: ", err);
+  });
--- airbab/controllers/authController.js
+++ airbab/controllers/authController.js
@@ -8,7 +8,7 @@ exports.getLogin = (req, res, next) => {
 
 exports.postLogin = (req, res, next) => {
   console.log(req.body);
-  res.cookie("isLoggedIn",true);
+  req.session.isLoggedIn = true;
   res.redirect("/");
 };
 
